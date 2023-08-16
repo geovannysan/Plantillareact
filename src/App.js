c64bd0b2e3232ff4c4d71bf113ea169bed1fe8aa
geovannysan <geovanny1santiago@gmail.com>
2023-08-15 22:22:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchAppBar from './components/Toolbar';
 
 function App() {
   return (
     <div className="App">
+      <SearchAppBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
